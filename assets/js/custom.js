221aaeccf6dc8a9e12ae135050ac51485ad8582c
ataurwub <ataurrahman.wub@gmail.com>
2022-10-27 01:40:14 +0600

    updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -109,6 +109,54 @@ $(document).ready(function () {
     // banner_simple_slider End
 
 
+    // department_area_slider Start 
+    $('.department_slider_wrap').slick({
+        arrows: true,
+        prevArrow: ".department_area .left_btn",
+        nextArrow: ".department_area .right_btn",
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        // centerMode: true,
+        // variableWidth: true,
+        adaptiveHeight: true,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        fade: false,
+        cssEase: 'linear',
+        // vertical: true,
+        // focusOnSelect: true,
+        // responsive: [
+        //     {
+        //     breakpoint: 1024,
+        //     settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 3,
+        //         infinite: true,
+        //         dots: true
+        //     }
+        //     },
+        //     {
+        //     breakpoint: 600,
+        //     settings: {
+        //         slidesToShow: 2,
+        //         slidesToScroll: 2
+        //     }
+        //     },
+        //     {
+        //     breakpoint: 480,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1
+        //     }
+        //     }
+        // ]
+    });
+    // department_area_slider End
+
+
 // OFFCANVUS  RIGHT PANEL JS START 
 
     
